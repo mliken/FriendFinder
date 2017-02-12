2b86a951da9c998396f019f851be76f4bf5c0b80
megan liken <meganliken@megans-MacBook-Pro-2.local>
2017-02-12 16:05:00 -0600

    new repo

---
+++ app/data/database.js
@@ -0,0 +1,27 @@
+//Exports the data of the full JSON object to be called by the web app.
+exports.data = [{
+  name: "Richard Hendricks",
+  answersArray: [3,3,3,4,4,2,4,2,0,2],
+  photo: "http://b.looklive.com/s/Thomas-Middleditch-Richard-Hendricks-Silicon-Valley-S02E04-The-Lady_L2VJNnh5alg4QjVaY2otMG04V01iV0JTRmlZOD0vNDAxeDA6MTEyMXgxMDgwLzY0MHgwL2ZpbHRlcnM6d2F0ZXJtYXJrKDIwY2U5ODc5LTYxOTctNDI4Ni1iYmY4LTE3MTg1M2JkM2RlZSwzOTAsNzgyLDEwKS9hNjYxMWU4Yi1hMTFiLTRjNDUtOGE3NC1hYzM0MmI0ODFjMTA=",
+}, {
+  name: "Erlich Bachman",
+  answersArray: [2,2,1,3,3,4,0,1,4,2],
+  photo: "https://b.fastcompany.net/multisite_files/fastcompany/imagecache/slideshow_large/slideshow/2015/05/3046707-slide-s-1-how-to-meet-ladies-decoding-the-magic-and-messages-baked-into-ehrlich-bachma.jpg",
+}, {
+  name: "Bertram Gilfoyle",
+  answersArray: [3,4,2,1,1,0,3,4,3,4],
+  photo: "http://c.looklive.com/ipU1qicCqB2pa0bT-5AlF5RzgOE=/0x0:720x1080/filters:watermark(20ce9879-6197-4286-bbf8-171853bd3dee,470,902,10)/b697f955-5d65-4921-8387-14d17b314a4f",
+}, {
+  name: "Dinesh Chugtai",
+  answersArray: [4,4,0,0,1,0,3,4,2,4],
+  photo: "http://i.lv3.hbo.com/assets/images/people/n/kumail-nanjiani-1024.jpg",
+}, {
+  name: "Gavin Belson",
+  answersArray: [1,3,2,1,3,4,3,0,2,2],
+  photo: "https://s3-us-west-2.amazonaws.com/assets.coderag.prd/wp-content/uploads/2016/03/nucleus.png"
+
+},{
+  name: "Jared Dunn",
+  answersArray: [4,3,2,2,3,4,3,2,2,2],
+  photo: "http://previously.tv/m/2014-04-13-silicon-valley.jpg",
+}];
